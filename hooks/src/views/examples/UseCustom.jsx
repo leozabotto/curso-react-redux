80e800732b93dd58f28d8072f9f845a604e6398d
Leonardo Zabotto <leozabotto02@gmail.com>
2022-04-10 00:15:44 -0300

    React Hooks (Custom Hooks)

--- hooks/src/views/examples/UseCustom.jsx
+++ hooks/src/views/examples/UseCustom.jsx
@@ -1,15 +1,54 @@
 import React from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
 
-const UseRef = (props) => {
+import { useCounter } from '../../hooks/useCustom'
+import { useFetch } from '../../hooks/useFetch'
+
+const UseCustom = (props) => {
+
+    const [count, inc, dec] = useCounter();
+    
+    const states = useFetch('http://files.cod3r.com.br/curso-react/estados.json')
+
+    function showStates(states) {
+        return states.data.map(state => <li>{state.nome} - {state.sigla}</li>)
+    }
+
     return (
         <div className="UseCustom">
             <PageTitle
                 title="Seu Hook"
                 subtitle="Vamos aprender como criar o nosso próprio Hook!"
             />
+
+            <SectionTitle title="Exercício #01" />
+            
+            <div className="center">
+                <span className="text">
+                    {count}
+                </span>
+
+                <div>
+                    <button className="btn" onClick={() => dec()}>
+                        -1
+                    </button>
+                    <button className="btn" onClick={() => inc()}>
+                        +1
+                    </button>
+                </div>
+            </div>
+
+            <SectionTitle title="Exercício #02" />
+
+            <div className="center">
+                <ul>
+                    {states.data ? showStates(states) : ''}
+                </ul>
+            </div>
+
         </div>
     )
 }
 
-export default UseRef
+export default UseCustom
